5f6b758001645117d86e6601d2a20917e25a53aa
SynthBadger <kmadrums@gmail.com>
2025-12-12 11:09:13 -0500

    Turn Off Fine Grained Nvidia

--- configuration.nix
+++ configuration.nix
@@ -38,7 +38,8 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.production;
